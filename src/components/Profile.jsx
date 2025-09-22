3c00d8669fb7b4de308c22c19895c412fc5a44ab
GURIVI REDDY YASASWINI <gyasu1110@gmail.com>
2025-09-23 00:11:47 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,7 @@ function Profile({ fullname }) {
   return (
     <div>
       <h1>Profile Component</h1>
-      <p>Email: {fullname}</p>
+      <p>Name: {fullname}</p>
       {/* Add more fields if you pass them as props */}
     </div>
   );
